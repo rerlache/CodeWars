1296ed74c66dc088a0fdb658c5da62c180a4c6bf
rerlache <rerlache@gmail.com>
2022-07-15 10:59:50 +0200

    updated sql challenge

---
+++ SQL/SQLBasics-RaiseToThePower.sql
@@ -0,0 +1,12 @@
+-- https://www.codewars.com/kata/594a8f653b5b4e8f3d000035
+
+-- Given the following table 'decimals':
+-- 
+-- decimals table schema
+-- * id
+-- * number1
+-- * number2
+-- Return a table with a single column result which is the output of number1 raised to the power of number2.
+
+SELECT POWER(number1, number2) as result
+FROM decimals
