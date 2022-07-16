d1a75290e1d5e77e34392f9be19b9aa8051625ba
Robin Erlacher <rerlache@gmail.com>
2022-07-16 17:48:09 +0200

    added BreakCamelCase challenge

---
+++ C#/BreakCamelCase/BreakCamelCase/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace BreakCamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(BreakCamelCase("breakCamelCase"));
+            Console.ReadLine();
+        }
+
+        static string BreakCamelCase(string str)
+        {
+            StringBuilder sb = new();
+            foreach (char item in str)
+            {
+                if (!char.IsUpper(item))
+                {
+                    sb.Append(item);
+                }
+                else
+                {
+                    sb.Append(" ");
+                    sb.Append(item);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ C#/BreakCamelCase/README.md
@@ -0,0 +1,12 @@
+## Source
+https://www.codewars.com/kata/5208f99aee097e6552000148
+
+## Description
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+## Example(s)
+```
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+```
